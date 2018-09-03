513dd3c0e5916455d1e72d557841a26b496355b4
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-03 17:34:58 +0500

    Add movie scaffold + root route

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_03_122309) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "movie_length"
+    t.string "director"
+    t.string "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

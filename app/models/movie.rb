a9096adcbb358f294cc2e5f162c73f801712f49a
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-04 10:59:35 +0500

    Add PaperClip gem + image to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  belongs_to :user
+
+  has_attached_file :image, styles: { medium: "400x600#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/./
 end
